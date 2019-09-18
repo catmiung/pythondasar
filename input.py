36fc263e1133afb8282d7223149b1295e3ed31f4
tekomc <55471530+tekomc@users.noreply.github.com>
2019-09-18 11:31:16 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,4 @@
+print "halo UNM"
+print "Nama Saya masukan nama anda"
+print "Nim Saya masukannim anda"
+print "saya bangga menjadi mahasiswa program studi teknik komputer"
---
+++ menghitungluassegitiga.py
@@ -0,0 +1,7 @@
+print("Menghitung Luas Segitiga") 
+t = float(input("Masukan tinggi : ")) 
+a = float(input("Masukan alas : ")) 
+luas = 0.5*a*t 
+print("Luas segitiga =",luas,"cm2")
+gambar hasilnya
+https://prnt.sc/p7joh0
